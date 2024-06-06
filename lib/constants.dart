6fbfdd9307d77aeb95369be889dd226d882c9c36
Daniel Fieni Pereira <daniel_fieni@hotmail.com>
2024-06-06 09:57:34 -0300

    changes detail page

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,15 +1,17 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Constants {
-  //Primary color
+  // Primary color
   static var primaryColor = const Color.fromRGBO(184, 143, 113, 1);
   static var blackColor = const Color.fromRGBO(0, 0, 0, 1);
-  static var white = const Color.fromRGBO(255,255,255,1);
+  static var white = const Color.fromRGBO(255, 255, 255, 1);
 
-  static var darkGrey = const Color.fromRGBO(33,37,40,1); 
-  //Onboarding texts
+  static var darkGrey = const Color.fromRGBO(33, 37, 40, 1);
+  static var naturalGrey = const Color.fromRGBO(56, 64, 70, 1);
+
+  // Onboarding texts
   static var titleOne = "Learn more about rocks";
   static var descriptionOne = "Read how to care for rocks in our rich rocks guide.";
   static var titleTwo = "Find a rock lover friend";
@@ -18,5 +20,56 @@ class Constants {
   static var descriptionThree = "Find almost all types of rocks that you like here.";
 
   static String GPT_API_KEY = dotenv.env['GPT_API_KEY'] ?? '';
+}
+
+class AppCollors {
+  // C6CCCA
+  static Color naturalSilver = const Color(0xFFC6CCCA);
+  // B88F71
+  static Color primaryMedium = const Color(0xFFB88F71);
+  // White
+  static Color white = const Color(0xFFFFFFFF);
+  // 1A1918
+  static Color naturalBlack = const Color(0xFF1A1918);
+  // 7E4E2B
+  static Color primaryDarkest = const Color(0xFF7E4E2B);
+}
+
+class AppTypography {
+  static TextStyle headline1({Color color = Colors.black}) {
+    return GoogleFonts.bebasNeue(
+      textStyle: TextStyle(
+        fontSize: 36,
+        fontWeight: FontWeight.normal,
+        color: color,
+      ),
+    );
+  }
+
+  static TextStyle headline2({Color color = Colors.black}) {
+    return GoogleFonts.bebasNeue(
+      textStyle: TextStyle(
+        fontSize: 28,
+        fontWeight: FontWeight.normal,
+        color: color,
+      ),
+    );
+  }
+
+  static TextStyle body3({ 
+    Color color = Colors.black,
+    TextDecoration decoration = TextDecoration.none,
+    Color decorationColor = Colors.black
+  }) {
+    return GoogleFonts.montserrat(
+      textStyle: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.normal,
+        color: color,
+        decoration: decoration,
+        decorationColor: decorationColor
+      ),
+    );
+  }
 
 }
--- lib/ui/screens/detail_page.dart
+++ lib/ui/screens/detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_onboarding/constants.dart';
 import 'package:flutter_onboarding/models/rocks.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RockDetailPage extends StatelessWidget {
   final Rock rock;
@@ -37,34 +38,70 @@ class RockDetailPage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Center(
-                child: Column(
-                  children: [
-                    Image.asset('assets/images/rock1.png', height: 200), // Placeholder image
-                    SizedBox(height: 10),
-                    Text(
-                      rock.rockName,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Constants.darkGrey,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/rock1.png',
+                        height: 200,
+                        width: 200,
+                      ), // Placeholder image
+                      Divider(
+                        color: Constants.naturalGrey,
+                        thickness: 1,
+                        // endIndent: 50,
+                        // indent: 50,
                       ),
-                    ),
-                    Text(
-                      'a variety of ${rock.category}',
-                      style: TextStyle(
-                        color: Colors.white70,
-                        fontSize: 16,
+                      const SizedBox(
+                        height: 10,
                       ),
-                    ),
-                  ],
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            rock.rockName,
+                            style: AppTypography.headline1(
+                                color: Constants.primaryColor),
+                          ),
+                          Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: 'a variety of ',
+                                  style: AppTypography.body3(
+                                      color: AppCollors.naturalSilver),
+                                ),
+                                TextSpan(
+                                  text: rock.category,
+                                  style: AppTypography.body3(
+                                      color: AppCollors.primaryMedium,
+                                      decoration: TextDecoration.underline,
+                                      decorationColor:
+                                          AppCollors.primaryMedium),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          _buildInfoSection('Formula', rock.formula),
+                          _buildInfoSection(
+                              'Hardness', rock.hardness.toString()),
+                          _buildInfoSection('Color', rock.color),
+                          _buildInfoSection('Magnetic',
+                              rock.isMagnetic ? 'Magnetic' : 'Non-magnetic'),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              SizedBox(height: 20),
-              _buildInfoSection('Formula', rock.formula),
-              _buildInfoSection('Hardness', rock.hardness.toString()),
-              _buildInfoSection('Color', rock.color),
-              _buildInfoSection('Magnetic', rock.isMagnetic ? 'Magnetic' : 'Non-magnetic'),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               _buildPremiumSection(),
               SizedBox(height: 20),
               _buildHealthRisksSection(),
@@ -87,16 +124,10 @@ class RockDetailPage extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(
-            title,
-            style: TextStyle(
-              color: Constants.primaryColor,
-              fontSize: 16,
-            ),
-          ),
+          Text(title, style: AppTypography.body3(color: AppCollors.white)),
           Text(
             value,
-            style: TextStyle(
+            style: const TextStyle(
               color: Colors.white,
               fontSize: 16,
             ),
@@ -108,25 +139,88 @@ class RockDetailPage extends StatelessWidget {
 
   Widget _buildPremiumSection() {
     return Container(
-      padding: const EdgeInsets.all(10),
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
       decoration: BoxDecoration(
-        color: Constants.darkGrey,
-        borderRadius: BorderRadius.circular(10),
+        gradient: const LinearGradient(
+          colors: [
+            // B88F71
+            Color(0xFFB88F71),
+            // A16132
+            Color(0xFFA16132),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(16),
       ),
       child: Row(
+        crossAxisAlignment:
+            CrossAxisAlignment.center, // Ajuste o alinhamento vertical
         children: [
-          Icon(Icons.star, color: Colors.amber),
-          SizedBox(width: 10),
-          Expanded(
-            child: Text(
-              'Try ROCKAPP Premium for free\nClaim your offer now',
-              style: TextStyle(color: Colors.white),
+          Icon(Icons.star_border, color: AppCollors.naturalBlack, size: 40),
+          const SizedBox(width: 10), // Espaçamento entre o ícone e o texto
+          Flexible(
+            fit: FlexFit.loose,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'Go ',
+                      style: GoogleFonts.montserrat(
+                          textStyle: TextStyle(
+                        color: AppCollors.naturalBlack,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                      )),
+                    ),
+                    SizedBox(width: 4,),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                      decoration: BoxDecoration(
+                        color: AppCollors.white,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Text(
+                        'Premium',
+                        style: GoogleFonts.montserrat(
+                          textStyle: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: AppCollors.primaryDarkest
+                          )
+                        )
+                      ),
+                    )
+                  ],
+                ),
+                // const Text('Go Premium'),
+                const SizedBox(
+                    height: 5), // Espaçamento entre o botão e o texto
+                Text(
+                  'Try ROCKAPP Premium for free',
+                  style: GoogleFonts.montserrat(
+                    textStyle: TextStyle(
+                      color: AppCollors.naturalBlack,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    )
+                  ),
+                ),
+                Text(
+                  'Claim your offer now',
+                  style: GoogleFonts.montserrat(
+                    textStyle: TextStyle(
+                      color: AppCollors.naturalBlack,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 12,
+                    )
+                  ),
+                )
+                // Claim your offer now
+              ],
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(primary: Colors.amber),
-            child: Text('Go Premium'),
           ),
         ],
       ),
@@ -176,7 +270,7 @@ class RockDetailPage extends StatelessWidget {
         Row(
           children: [
             _buildImageCard('Quartz', 'assets/images/emerald.png'),
-            _buildImageCard('Quartz', 'assets/images/emerald1.png'),
+            _buildImageCard('Quartz', 'assets/images/emerald2.png'),
           ],
         ),
       ],
@@ -208,9 +302,9 @@ class RockDetailPage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        SizedBox(height: 10),
+        const SizedBox(height: 10),
         // Replace with actual map widgets or images
-        Image.asset('assets/map.png', height: 150), // Placeholder image
+        Image.asset('assets/images/map.png', height: 150), // Placeholder image
       ],
     );
   }

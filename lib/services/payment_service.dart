82a9812ce5ee46df6c71096fa4582a7f7de4211b
Daniel Fieni Pereira <daniel_fieni@hotmail.com>
2024-08-19 10:43:58 -0300

    fix: Configuration StoreKit file

--- lib/services/payment_service.dart
+++ lib/services/payment_service.dart
@@ -58,15 +58,20 @@ class PaymentService {
       return true;
     } catch (e) {
       debugPrint("Error: $e");
-      if (e is PlatformException) {
-        showToast("${e.message} Please try again later", context);
-      } else {
-        showToast(
-          localizationService.getString(LocalizedString.errorPleaseTryAgain),
-          context,
-        );
-      }
+      String errorMessage = 'An error occurred. Please try again.';
 
+      if (e is PlatformException && e.code == '21007') {
+        errorMessage = localizationService
+            .getString(LocalizedString.sandboxReceiptUsedInProduction);
+      } else if (e is PlatformException) {
+        errorMessage = e.message!;
+      } else if (e is Exception) {
+        errorMessage = e.toString();
+      }
+      await showToast(
+        errorMessage,
+        context,
+      );
       return false;
     }
   }
